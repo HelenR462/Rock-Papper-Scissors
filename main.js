dc4fca7ac93b7ea009712d9262225cd1f4b6b849
HelenR462 <69416811+HelenR462@users.noreply.github.com>
2022-05-15 17:15:26 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// what makes the button click?
+// How does the computer pick its choice?
+// how do I match the outcomes to make a decision of win or lose?
+//how do I transfer the score to show on the score counter?
+// how do I declair the winner
+
+let myFunction = addEventListener(onclick, myFunction());
+let computerPick = Math.random();
+let selection = ["Rock","Paper","Scissors"];
+
+function myFunction(computerPick){
+    Math.random();
+    return selection;
+}
+
+let playerChoice = selection;
+selection =["Rock","Paper","Scissors"];
+
+function myFunction(playerChoice){
+if (selection === computerPick)
+     return "It's a Tie!";
+}
+
+let score = [1,2,3,4,5,6,7,8,9,10];
+let playerScore;
+let computerScore;
+
+function myFunction(score){
+    if (playerScore === computerScore)
+    return "it's a Tie!";
+} else if {
+    (playerScore > computerScore)
+    return "Player Wins!";
+}else{
+    (playerScore > computerScore)
+    return "You Lose):"
+}
+
+
+
+
