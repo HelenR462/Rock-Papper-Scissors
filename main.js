accd257b4c013dce75bae2a5c5983b809c06e42c
HelenR462 <69416811+HelenR462@users.noreply.github.com>
2022-05-27 11:04:33 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,58 @@
-// what makes the button click?
+document.getElementById('rock').addEventListener("click", rock);
+document.getElementById('paper').addEventListener("click", paper);
+document.getElementById('scissors').addEventListener("click", scissors);
 
-let Rock = document.getElementById("rock").addEventListener("click", play);
-let Papper = document.getElementById("papper").addEventListener("click", play);
-let Scissors = document.getElementById("scissors").addEventListener("click", play);
-  
-function playFunction(){
-  
-  let play = pickSelection;
-  let playerPick;
-  let computerPick;
-  const Selection = ["rock", "papper","scissors"];
 
-    return Selection;
-    
+ function rock(){
+ document.getElementById('rock'). innerHTML = 'rock';
+  }
+
+ function paper(){
+ document.getElementById('paper').innerHTML = 'paper'; 
 }
+
+ function scissors(){
+ document.getElementById('scissors').innerHTML = 'sissors'; 
+ }
+
+ let playerScore = 0;
+ let computerScore = 0;
+ let playerChoice;
+ let computerChoice ;
+ let selections;
+ 
+ 
+ function computer(){
+    playerChoice = ["rock", "paper", "scissors"]
+        computerChoice =playerChoice[Math. floor(Math. random() * playerChoice. length)]  
+         
+    console.log(computerChoice);
+         } 
+
+ function compare(){        
+    let tie =[["rock","rock"],
+            ["paper","paper"],
+            ["scissors","scissors"]];
+
+    let win =[["rock","scissors"],
+            ["paper","rock"],
+            ["scissors","paper"]];
+
+    let lose= [["rock","paper"],
+            ["paper","scissors"],["scissors","rock"]];
+          
+            print[('You chose' + 'playerChoice') + ('Computer chose' + 'computerChoice')];
+ }
+         
+         switch(playerChoice, computerChoice) {
+            case tie  :
+               print("It's a Tie!");
+        break;
+             case win:
+               print(" You Win!");
+        break;
+              case lose:
+               print(" You Lose!");
+          
+        }
+    
